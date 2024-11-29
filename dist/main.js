34719582189fd06df9266c2ee2057cd76b95ae94
Hieu.mh7 <hieu.mh7@gmail.com>
2024-11-29 09:58:35 +0700

    sua 1 chut

--- dist/main.js
+++ dist/main.js
@@ -2,10 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const platform_express_1 = require("@nestjs/platform-express");
+const express = require("express");
+const server = express();
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(server));
     app.enableCors();
-    await app.listen(3000);
+    await app.init();
 }
 bootstrap();
+exports.default = server;
 //# sourceMappingURL=main.js.map
